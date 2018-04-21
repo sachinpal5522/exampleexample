e5b0f613ce669a74466c3c6d043c84801b5446bf
sachin <sachinpal552@gmail.com>
2018-04-21 12:12:44 +0530

    graph changed to pie

--- shopmanagement/Dashboard.Designer.cs
+++ shopmanagement/Dashboard.Designer.cs
@@ -32,10 +32,10 @@
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.Btnpanel = new System.Windows.Forms.Panel();
+            this.categorybtn = new System.Windows.Forms.Button();
+            this.productbtn = new System.Windows.Forms.Button();
             this.graphpanel = new System.Windows.Forms.Panel();
             this.masalachat = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.productbtn = new System.Windows.Forms.Button();
-            this.categorybtn = new System.Windows.Forms.Button();
             this.Btnpanel.SuspendLayout();
             this.graphpanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.masalachat)).BeginInit();
@@ -51,31 +51,20 @@
             this.Btnpanel.Size = new System.Drawing.Size(164, 418);
             this.Btnpanel.TabIndex = 0;
             // 
-            // graphpanel
+            // categorybtn
             // 
-            this.graphpanel.Controls.Add(this.masalachat);
-            this.graphpanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.graphpanel.Location = new System.Drawing.Point(164, 0);
-            this.graphpanel.Name = "graphpanel";
-            this.graphpanel.Size = new System.Drawing.Size(706, 418);
-            this.graphpanel.TabIndex = 1;
-            // 
-            // masalachat
-            // 
-            chartArea1.Name = "ChartArea1";
-            this.masalachat.ChartAreas.Add(chartArea1);
-            this.masalachat.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Name = "Legend1";
-            this.masalachat.Legends.Add(legend1);
-            this.masalachat.Location = new System.Drawing.Point(0, 0);
-            this.masalachat.Name = "masalachat";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Profit";
-            this.masalachat.Series.Add(series1);
-            this.masalachat.Size = new System.Drawing.Size(706, 418);
-            this.masalachat.TabIndex = 0;
-            this.masalachat.Text = "chart1";
+            this.categorybtn.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.categorybtn.FlatAppearance.BorderSize = 0;
+            this.categorybtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.categorybtn.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categorybtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.categorybtn.Location = new System.Drawing.Point(13, 189);
+            this.categorybtn.Name = "categorybtn";
+            this.categorybtn.Size = new System.Drawing.Size(140, 40);
+            this.categorybtn.TabIndex = 4;
+            this.categorybtn.Text = "Category Wise";
+            this.categorybtn.UseVisualStyleBackColor = false;
+            this.categorybtn.Click += new System.EventHandler(this.categorybtn_Click);
             // 
             // productbtn
             // 
@@ -92,20 +81,40 @@
             this.productbtn.UseVisualStyleBackColor = false;
             this.productbtn.Click += new System.EventHandler(this.productbtn_Click);
             // 
-            // categorybtn
+            // graphpanel
             // 
-            this.categorybtn.BackColor = System.Drawing.Color.MediumSeaGreen;
-            this.categorybtn.FlatAppearance.BorderSize = 0;
-            this.categorybtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.categorybtn.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.categorybtn.ForeColor = System.Drawing.SystemColors.Control;
-            this.categorybtn.Location = new System.Drawing.Point(13, 189);
-            this.categorybtn.Name = "categorybtn";
-            this.categorybtn.Size = new System.Drawing.Size(140, 40);
-            this.categorybtn.TabIndex = 4;
-            this.categorybtn.Text = "Category Wise";
-            this.categorybtn.UseVisualStyleBackColor = false;
-            this.categorybtn.Click += new System.EventHandler(this.categorybtn_Click);
+            this.graphpanel.Controls.Add(this.masalachat);
+            this.graphpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.graphpanel.Location = new System.Drawing.Point(164, 0);
+            this.graphpanel.Name = "graphpanel";
+            this.graphpanel.Size = new System.Drawing.Size(706, 418);
+            this.graphpanel.TabIndex = 1;
+            // 
+            // masalachat
+            // 
+            chartArea1.AxisX.IsLabelAutoFit = false;
+            chartArea1.AxisX.IsMarksNextToAxis = false;
+            chartArea1.AxisX.LabelAutoFitStyle = ((System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles)((((((System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.IncreaseFont | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.DecreaseFont) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.StaggeredLabels) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.LabelsAngleStep30) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.LabelsAngleStep45) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAutoFitStyles.LabelsAngleStep90)));
+            chartArea1.Name = "ChartArea1";
+            this.masalachat.ChartAreas.Add(chartArea1);
+            this.masalachat.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.masalachat.Legends.Add(legend1);
+            this.masalachat.Location = new System.Drawing.Point(0, 0);
+            this.masalachat.Name = "masalachat";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Profit";
+            this.masalachat.Series.Add(series1);
+            this.masalachat.Size = new System.Drawing.Size(706, 418);
+            this.masalachat.TabIndex = 0;
+            this.masalachat.Text = "chart1";
             // 
             // Dashboard
             // 
